f843235942741ca4a6bff3d4dc2a1023c2edd49f
futrue-001 <3446563198@qq.com>
2023-06-21 18:46:54 +0800

    模拟分支冲突的情况(main提交)[Main分支进行更改]

--- demo/src/com/xj/Test4.java
+++ demo/src/com/xj/Test4.java
@@ -3,5 +3,11 @@ package com.xj;
 public class Test4 {
     public static void main(String[] args) {
         System.out.println("展示分支冲突的情况 test4");
+
+        System.out.println("main分支进行修改");
+
+        String a = "xj";
+
+        System.out.println(a);
     }
 }
